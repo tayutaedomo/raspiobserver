c4a28990cfd4e281d0f3f90f48767c20ac77bc25
tayutaedomo <tayutaedomo@gmail.com>
2020-12-08 00:35:29 +0900

    Add upload_temperature_metrics.py

---
+++ applib/uploader.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+ROOT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+ROOT_PATH = os.path.abspath(ROOT_PATH)
+sys.path.append(os.path.abspath(ROOT_PATH))
+
+import applib.gcs
+
+
+class TemperatureMetricsLogUploader:
+    def __init__(self):
+        self.log_name = 'raspiobserver_temp_metrics.csv'
+        self.log_path = os.path.join('/var/tmp/', self.log_name)
+        self.gcs_dir = 'temperature'
+
+    def upload(self, target_date):
+        local_path = self.log_path + '.' + target_date
+
+        if not os.path.exists(local_path):
+            raise FileNotFoundError()
+
+        gcs_file_name = self.log_name.replace('.'+'target_date', '')
+        gcs_file_name = target_date + '_' + gcs_file_name
+        gcs_path = self.gcs_dir + '/' + gcs_file_name
+
+        applib.gcs.upload(gcs_path, local_path)
+
+
+
+if __name__ == '__main__':
+    uploader = TemperatureMetricsLogUploader()
+    uploader.upload('2020-12-06')
+
